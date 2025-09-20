2808fa7125029f877d4696e282122659b22f0563
KelvinRide <jptheron99@gmail.com>
2025-09-20 11:50:23 +0200

    Chapter 11 pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -14,6 +14,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
     const params = new URLSearchParams(searchParams);
     
+    params.set('page','1');
     if (term?.length > 2) {
       params.set('query', term);
     } else {
